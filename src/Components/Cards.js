d80559c4b391afc20b98dd80fe2909d5f529898a
Mahmoud Jarjour <Mahmoudkarakie1994m@gmail.com>
2021-10-21 11:39:31 +0300

    adjust styles

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -6,29 +6,27 @@ import i18next from 'i18next';
 import i18n from '../Locales/i18n';
 import { orange } from '@mui/material/colors';
 
-
-
 const getLanguage = () => i18next.language || window.localStorage.i18nextLng;
 const data = [
 	{
 		id: 1,
 		discription: 'Imprisonment, preventive detention or a ne of more than ve hundred thousand Lebanese pounds.',
-		color:'#8CBED6',
+		color: '#A1CAF1',
 	},
 	{
 		id: 2,
 		discription: 'Additional or secondary penalty or personal compensations of more than ve hundred thousand Lebanese pounds.',
-		color:'#CC5500'
+		color: '#FF9966',
 	},
 	{
 		id: 3,
 		discription: 'Revocation of defense stipulated in Article 73 of the Criminal Procedures Code.',
-		color:'#800020'
+		color: '#FF91AF',
 	},
 	{
 		id: 4,
 		discription: 'Penalty for a violation correlative with a misdemeanor.',
-		color:'#87A96B'
+		color: '#87A96B',
 	},
 ];
 
@@ -36,45 +34,45 @@ const dataAr = [
 	{
 		id: 1,
 		discription: 'الحبس زأو التوقيف التكديري أو غرامة تزيد على ٥٠٠ ألف ليرة لبنانية',
-		color:'#8CBED6',
-		
+		color: '#A1CAF1',
 	},
 	{
 		id: 2,
 		discription: 'عقوبة إضافية أو فرعية أو تعويضات شخصية تزيد على الخمسمائة ألف ليرة لبنانية',
-		color:'#CC5500'
+		color: '#FF9966',
 	},
 	{
 		id: 3,
 		discription: 'رد من الدفوع المنصوص عليها في المادة ٧٣ من قانون أصول المحاكمات الجزائية',
-		color:'#800020'
+		color: '#FF91AF',
 	},
 	{
 		id: 4,
 		discription: 'عقوبة لمخالفة متلازمة مع جنحة',
-		color:'#87A96B'
+		color: '#87A96B',
 	},
 ];
-
 
 const Cards = ({ card }) => {
 	const { t } = useTranslation();
 	return (
 		<Container>
 			<Grid container spacing={15} columns={16}>
-				{getLanguage() === 'ar'? dataAr.map((item) => (
-					<Grid item xs={4} key={item.id}>
-						<Typography backgroundColor={item.color} borderRadius>
-							{item.discription}
-						</Typography>
-					</Grid>
-				)) : data.map((item) => (
-					<Grid item xs={4} key={item.id}>
-						<Typography backgroundColor={item.color} borderRadius>
-							{item.discription}
-						</Typography>
-					</Grid>
-				))}
+				{getLanguage() === 'ar'
+					? dataAr.map((item) => (
+							<Grid item xs={4} key={item.id}>
+								<Typography backgroundColor={item.color} borderRadius>
+									{item.discription}
+								</Typography>
+							</Grid>
+					  ))
+					: data.map((item) => (
+							<Grid item xs={4} key={item.id}>
+								<Typography backgroundColor={item.color} borderRadius>
+									{item.discription}
+								</Typography>
+							</Grid>
+					  ))}
 			</Grid>
 			<Typography variant="h5">
 				<PushPinTwoToneIcon color="success" fontSize="large" />
