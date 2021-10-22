60acf8d40aea5a4813335bf373e46e35c47d6373
Mahmoud Jarjour <Mahmoudkarakie1994m@gmail.com>
2021-10-22 16:08:21 +0300

    adjust styles

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -13,26 +13,30 @@ const data = [
 	{
 		id: 1,
 		discription: 'Imprisonment, preventive detention or a ne of more than ve hundred thousand Lebanese pounds.',
-		color: '#F5F5F5',
-		border: '2px solid #e35e94',
+		color: '#511a15',
+		border: '1px solid',
+		backgroundColor:'#fdecea'
 	},
 	{
 		id: 2,
 		discription: 'Additional or secondary penalty or personal compensations of more than ve hundred thousand Lebanese pounds.',
-		color: '#F5F5F5',
-		border: '2px solid #93d2b9',
+		color: '#1e4620',
+		border: '1px solid',
+		backgroundColor:'#edf7ed'
 	},
 	{
 		id: 3,
 		discription: 'Revocation of defense stipulated in Article 73 of the Criminal Procedures Code.',
-		color: '#F5F5F5',
-		border: '2px solid #93d2b9',
+		color: '#511a15',
+		border: '1px solid',
+		backgroundColor:'#fdecea'
 	},
 	{
 		id: 4,
 		discription: 'Penalty for a violation correlative with a misdemeanor.',
-		color: '#F5F5F5',
-		border: '2px solid #e35e94',
+		color: '#1e4620',
+		border: '1px solid',
+		backgroundColor:'#edf7ed'
 	},
 ];
 
@@ -40,26 +44,30 @@ const dataAr = [
 	{
 		id: 1,
 		discription: 'الحبس أو التوقيف التكديري أو غرامة تزيد على ٥٠٠ ألف ليرة لبنانية',
-		color: '#F5F5F5',
-		border: '2px solid #e35e94',
+		color: '#611a15',
+		border: '2px solid #611a15',
+		backgroundColor:'#fdecea'
 	},
 	{
 		id: 2,
 		discription: 'عقوبة إضافية أو فرعية أو تعويضات شخصية تزيد على الخمسمائة ألف ليرة لبنانية',
-		color: '#F5F5F5',
-		border: '2px solid #93d2b9',
+		color: '#1e4620',
+		border: '2px solid #1e4620',
+		backgroundColor:'#edf7ed'
 	},
 	{
 		id: 3,
 		discription: 'رد من الدفوع المنصوص عليها في المادة ٧٣ من قانون أصول المحاكمات الجزائية',
-		color: '#F5F5F5',
-		border: '2px solid #93d2b9',
+		color: '#1e4620',
+		border: '2px solid #1e4620',
+		backgroundColor:'#edf7ed'
 	},
 	{
 		id: 4,
 		discription: 'عقوبة لمخالفة متلازمة مع جنحة',
-		color: '#F5F5F5',
-		border: '2px solid #e35e94',
+		color: '#611a15',
+		border: '2px solid #611a15',
+		backgroundColor:'#fdecea'
 		
 	},
 ];
@@ -85,10 +93,11 @@ const Cards = ({ card }) => {
 										alignContent: 'center',
 									}}
 									border={item.border}
-									backgroundColor='white'
+									backgroundColor={item.backgroundColor}
+									color={item.color}
 									
 								>
-									
+								
 									 {item.discription}
 									 
 								</Grid>
@@ -110,7 +119,8 @@ const Cards = ({ card }) => {
 										alignContent: 'center',
 									}}
 									border={item.border}
-									backgroundColor='white'
+									backgroundColor={item.backgroundColor}
+									color={item.color}
 									
 								>
 									{item.discription}
@@ -123,7 +133,7 @@ const Cards = ({ card }) => {
 			<Grid xs={12} sm={12} md={12} lg={12} >
 			<Divider variant="middle" orientation='horizontal' style={{ paddingBottom:'10px'}}/>
 			
-						<Typography style={{backgroundColor:'#fff4e5',color:'#663C01', fontFamily: 'Cairo', paddingBottom: '20px', paddingTop: '60px', fontWeight:'500' , fontSize:'23px' }} variant="h5">
+						<Typography style={{backgroundColor:'#fff4e5',color:'#663C01', fontFamily: 'Cairo', paddingBottom: '20px', paddingTop: '60px', fontWeight:'500' , fontSize:'23px',padding:'15px' }} variant="h5">
 						<TaskAltIcon style={{color:'#ff9800'}} />
 				{t(
 					'The Court of Appeal is entitled, when exercising its tasks, to listen to witnesses, interrogate the parties of the lawsuit, invite new witnesses, appoint an expert, listen to experts and carry out any new investigations it deems appropriate...'
